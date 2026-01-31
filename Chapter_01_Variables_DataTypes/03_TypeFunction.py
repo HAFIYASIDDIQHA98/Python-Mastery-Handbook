96802cf6d62bc702689ab1d10219c9ee64fb9bae
Hafiya Siddiqha <hafiyasoftwaredeveloper1@gmail.com>
2026-01-31 18:58:27 +0530

    feat: use type() to identify data types
    Explored Python’s built-in type() function. This script is used to programmatically identify the data class of any variable, which is a crucial debugging skill when working with dynamic data sources.

---
+++ Chapter_01_Variables_DataTypes/03_TypeFunction.py
@@ -0,0 +1,8 @@
+# Checking the type of variables
+a = 34
+b = "Python"
+c = 78.9
+
+print(type(a)) # Output: <class 'int'>
+print(type(b)) # Output: <class 'str'>
+print(type(c)) # Output: <class 'float'>
