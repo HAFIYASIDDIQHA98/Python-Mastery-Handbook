5451733fb8333ae1eef2054212be2ac36df94878
Hafiya Siddiqha <hafiyasoftwaredeveloper1@gmail.com>
2026-01-31 21:15:08 +0530

    feat: utilize set manipulation methods like add and remove
    Demonstrated basic Set operations, including adding new elements and removing existing ones. This script focuses on the dynamic nature of sets and the importance of error-free element management within a collection.

--- Chapter_04_Dictionary_Sets/04:Count_and_Index_in_Tuples.py
+++
@@ -1,4 +0,0 @@
-# Tuple Methods
-t = (1, 2, 3, 2, 4, 2)
-print("Count of 2:", t.count(2))
-print("Index of 4:", t.index(4)))
---
+++ Chapter_04_Dictionary_Sets/04_SetBasics.py
@@ -0,0 +1,3 @@
+# Sets: No duplicates allowed
+s = {1, 2, 3, 2, 1, 4}
+print(s) # Output: {1, 2, 3, 4} (Duplicates removed)
