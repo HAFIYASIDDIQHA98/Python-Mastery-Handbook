825189462b4fd91cc3a6102ac609927f24be3c81
Hafiya Siddiqha <hafiyasoftwaredeveloper1@gmail.com>
2026-02-01 22:44:09 +0530

    feat: implement dynamic string formatting using f-strings
    Showcased the modern way to inject variables into strings using f-strings. This makes the code much cleaner and easier to read compared to old-style concatenation, especially when building dynamic messages.

---
+++ Chapter_02_Strings_Conditionals/04_StringFormatting.py
@@ -0,0 +1,6 @@
+# F-Strings: Dynamic variables inside strings
+user = "Hafiya"
+language = "Python"
+# The cleanest way to format
+message = f"Hello {user}, welcome to the {language} world!"
+print(message)
