f72fd33e9fa5f28055cb0e4bd75ff5324c19c389
Hafiya Siddiqha <hafiyasoftwaredeveloper1@gmail.com>
2026-02-20 23:15:02 +0530

    feat: create a simple username and password check logic
    Simulates a login screen by comparing user input with stored credentials.

---
+++ challenges/49_Login_Mockup.py
@@ -0,0 +1,10 @@
+username = "hafiya"
+password = "password123"
+
+u_input = input("Enter Username: ")
+p_input = input("Enter Password: ")
+
+if u_input == username and p_input == password:
+    print("Login Successful!")
+else:
+    print("Invalid Credentials.")
