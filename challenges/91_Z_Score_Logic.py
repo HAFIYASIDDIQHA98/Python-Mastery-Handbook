d884e09cce3fe90a508bbde7a8de944dfdeb5520
Hafiya Siddiqha <hafiyasoftwaredeveloper1@gmail.com>
2026-03-03 19:05:28 +0530

    feat: calculate Z-Score for a data point
    Built a script to find the Z-score, determining how many standard deviations a value is from the mean.

---
+++ challenges/91_Z_Score_Logic.py
@@ -0,0 +1,7 @@
+value = 85
+mean = 70
+std_dev = 10
+
+z_score = (value - mean) / std_dev
+print(f"Value: {value}, Mean: {mean}, Std Dev: {std_dev}")
+print(f"Z-Score: {z_score} (This value is {z_score} standard deviations away from mean)")
